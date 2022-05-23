a96687ea78f2ae94282262f7b2e98151bfc6fb37
David <haz.d@outlook.com>
2022-05-23 12:19:05 +0300

    remove list util

--- lib/utils/List.jsx
+++
@@ -1,8 +0,0 @@
-const getRenderValue = (item) => typeof item === 'object' ? JSON.stringify(item) : item;
-const randomKey = () => Math.random().toString(36).substring(2, 10) + Math.random().toString(36).substring(2, 10);
-
-export const List = ({ from, fallback }) => {
-    if (Array.isArray(from) && from.length) {
-        return <ul>{from.map(item => <li key={randomKey}>{getRenderValue(item)}</li>)}</ul>
-    } else return fallback;
-}
