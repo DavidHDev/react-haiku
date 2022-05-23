e6ef17247142d7c8a75498c8e5585af1b3303f64
David <haz.d@outlook.com>
2022-05-23 12:17:12 +0300

    attempt fix

--- lib/utils/List.jsx
+++ lib/utils/List.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-
 const getRenderValue = (item) => typeof item === 'object' ? JSON.stringify(item) : item;
+const randomKey = () => Math.random().toString(36).substring(2, 10) + Math.random().toString(36).substring(2, 10);
 
 export const List = ({ from, fallback }) => {
     if (Array.isArray(from) && from.length) {
-        return <ul>{React.Children.toArray(from.map(item => <li>{getRenderValue(item)}</li>))}</ul>
+        return <ul>{from.map(item => <li key={randomKey}>{getRenderValue(item)}</li>)}</ul>
     } else return fallback;
 }
