4fce47bcd559bc9971759e010f02416a5ad3086f
Vinicius Bass <raylee558@gmail.com>
2023-07-10 23:12:08 +0100

    refact to make simpler, add necessary imports and types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -132,7 +132,7 @@ export declare const useScrollPosition: () => [
 
 export declare const useSingleEffect: (effect: () => void) => void;
 
-export declare const useSize : (ref : MutableRefObject<Element | null>) => {width : number , height : number}
+export declare const useSize: (ref: MutableRefObject<Element | null>) => { width: number, height: number }
 
 export declare const useTitle: (title: string) => void;
 
@@ -156,6 +156,11 @@ export declare const useBatteryStatus: () => {
   level: number;
   isCharging: boolean;
 };
+
+export declare const useFullscreen: (targetRef: MutableRefObject<Element | null>) => {
+  isFullscreen: boolean,
+  toggleFullscreen: () => void
+}
 
 // utils
 
