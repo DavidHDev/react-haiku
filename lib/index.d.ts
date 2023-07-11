1172bd1a06841c78f369aa26d5c9d979299f23f4
Giovym <giovanni.marletta@outlook.com>
2023-07-11 20:15:30 +0200

    updated the index.js and index.d.ts files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,7 +4,7 @@ import {
   FC,
   MouseEventHandler,
   MutableRefObject,
-  ReactNode
+  ReactNode,
 } from "react";
 
 // hooks
@@ -35,7 +35,7 @@ export declare const useCookie: <T extends string | {}>(
 
 export declare const useCookieListener: <T extends string | {}>(
   effect: (cookieValue: T, key: string) => void,
-  cookieDeps?: string[],
+  cookieDeps?: string[]
 ) => void;
 
 export declare const useDebounce: <T>(value: T, delay?: number) => T;
@@ -132,7 +132,10 @@ export declare const useScrollPosition: () => [
 
 export declare const useSingleEffect: (effect: () => void) => void;
 
-export declare const useSize : (ref : MutableRefObject<Element | null>) => {width : number , height : number}
+export declare const useSize: (ref: MutableRefObject<Element | null>) => {
+  width: number;
+  height: number;
+};
 
 export declare const useTitle: (title: string) => void;
 
@@ -186,3 +189,14 @@ export declare const Show: FC<{
     render?: () => ReactNode;
   }>;
 };
+
+export declare const Image: FC<{
+  src: string;
+  loading?: "eager" | "lazy";
+  alt?: string;
+  fallback?: string;
+  style?: React.CSSProperties;
+  width?: number;
+  height?: number;
+  className?: string;
+}>;
