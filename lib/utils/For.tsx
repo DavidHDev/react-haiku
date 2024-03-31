9ea0f88715ddb4a14a852fc800f26459d1818f2f
Davi Silva <davi.psilva@midway.com.br>
2024-03-31 15:06:47 -0300

    separate types in type

--- lib/utils/For.tsx
+++ lib/utils/For.tsx
@@ -1,6 +1,6 @@
 import { Children, type ReactNode } from 'react';
 
-export const For = <T,>({render, each}: {
-    render: (item: T, index?: number) => ReactNode;
-    each: T[];
-}) => Children.toArray(each.map((item, index) => render(item, index)));
+type RenderFn = <T>(item: T, index?: number) => ReactNode;
+
+export const For = <T,>({ render, each }: { render: RenderFn; each: T[] }) =>
+  Children.toArray(each.map((item, index) => render(item, index)));
