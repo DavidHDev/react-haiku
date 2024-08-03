8292e408710777a412fd8264c2eeb7d077f8a452
James Jester <elmojester@gmail.com>
2024-08-03 02:22:44 -0500

    Update: Updated with type annotation and requested spacing.

--- lib/hooks/useWindowSize.ts
+++ lib/hooks/useWindowSize.ts
@@ -1,21 +1,26 @@
 import { useState, useEffect } from "react";
 
+type WindowSizeProps = {
+  width: number;
+  height: number;
+};
+
 export const useWindowSize = () => {
-    const [windowSize, setWindowSize] = useState({
-        width: window.innerWidth, 
-        height: window.innerHeight
-    });
+  const [windowSize, setWindowSize] = useState<WindowSizeProps>({
+    width: window.innerWidth, 
+    height: window.innerHeight
+  });
 
-    useEffect(() =>{
-        const handleResize = () => {
-            setWindowSize({ 
-                width: window.innerWidth, 
-                height: window.innerHeight
-            })
-        }
+  useEffect(() =>{
+    const handleResize = () => {
+    setWindowSize({ 
+      width: window.innerWidth, 
+      height: window.innerHeight
+    })
+  }
 
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, [])
-    return windowSize;
-}
+  window.addEventListener("resize", handleResize);
+  return () => window.removeEventListener("resize", handleResize);
+  }, [])
+  return windowSize;
+} 
