457fef917129f7a568fe04cad7afb811edabbb80
Tauseef Ansari <tauseeftanvir@gmail.com>
2024-08-07 20:01:21 +0530

    refactor(If): :recycle: Make `If` unit test dynamic

--- lib/utils/__tests__/If.test.tsx
+++ lib/utils/__tests__/If.test.tsx
@@ -4,14 +4,16 @@ import { takeSnapshot } from 'lib/helpers/testHelper';
 import { render, screen } from '@testing-library/react';
 
 describe('If', () => {
+  const text = 'Hello world!';
+
   it('renders the If component', () => {
-    takeSnapshot(<If isTrue>Hello</If>);
-    expect(screen.getByText('Hello')).toBeInTheDocument();
+    takeSnapshot(<If isTrue>{text}</If>);
+    expect(screen.getByText(text)).toBeInTheDocument();
   });
 
   it('should not render anything if isTrue is false', () => {
-    render(<If isTrue={false}>Hello</If>);
-    expect(screen.queryByText('Hello')).toBeNull();
-    expect(screen.queryByText('Hello')).not.toBeInTheDocument();
+    render(<If isTrue={false}>{text}</If>);
+    expect(screen.queryByText(text)).toBeNull();
+    expect(screen.queryByText(text)).not.toBeInTheDocument();
   });
 });
