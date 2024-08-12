b1c8887e458146c163c4e826fbf8c4f623dfee00
Tauseef Ansari <tauseeftanvir@gmail.com>
2024-08-12 14:59:08 +0530

    test(For): :white_check_mark: `For` Component unit test

---
+++ lib/utils/__tests__/For.test.tsx
@@ -0,0 +1,21 @@
+import { For } from '../For';
+import { render, screen } from '@testing-library/react';
+
+describe('For', () => {
+  const testArray = ['foo', 'bar', 'baz'];
+
+  it('renders the For component', () => {
+    const { asFragment } = render(
+      <For
+        each={testArray}
+        render={(item, index) => <span key={index}>{`${index}: ${item}`}</span>}
+      />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+
+    testArray.forEach((item, index) => {
+      expect(screen.getByText(`${index}: ${item}`)).toBeInTheDocument();
+    });
+    expect(document.body.firstChild!.childNodes).toHaveLength(testArray.length);
+  });
+});
